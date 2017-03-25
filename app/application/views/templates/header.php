5f424312b640692f3f3d461d28240b6c39312a8d
AndraLaura <andralaura11@gmail.com>
2017-03-25 15:09:07 +0200

    Esialgne

---
+++ app/application/views/templates/header.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+		<title>filmcritic</title>
+		<link rel="stylesheet" href="https://bootswatch.com/readable/bootstrap.min.css">
+	</head>
+	<body>
+	<nav class="navbar navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">filmcritic</a>
+        </div>
+        <div id="navbar">
+          <ul class="nav navbar-nav">
+            <li><a href="<?php echo base_url(); ?>">Home</a></li>
+            <li><a href="<?php echo base_url(); ?>browse">Browse</a></li>
+            <li><a href="<?php echo base_url(); ?>addmovie">Add movie</a></li>
+            <li><a href="<?php echo base_url(); ?>manage">Manage</a></li>
+            <li><a href="<?php echo base_url(); ?>about">About</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="<?php echo base_url(); ?>login">Sign in</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
