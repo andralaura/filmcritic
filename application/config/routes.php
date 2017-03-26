674e819a8ed1321297ef61cde49e6af5590fea2a
AndraLaura <andralaura11@gmail.com>
2017-03-26 22:27:39 +0300

    Routes muudatus vol2

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 
-$route['default_controller'] = 'pages';
+$route['default_controller'] = 'pages/view';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
