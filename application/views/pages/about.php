1d89a94cab469b3aa108e5afab04463acbfed5e0
AndraLaura <andralaura11@gmail.com>
2017-04-01 13:45:37 +0300

    Veebilehe sisu esitamine kaardil

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,1 +1,33 @@
 <h2><?= $title ?></h2>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+       #map {
+        height: 400px;
+        width: 50%;
+       }
+    </style>
+  </head>
+  <body>
+    <p>Kus me asume:</p>
+    <div id="map"></div>
+    <script>
+      function initMap() {
+        var uluru = {lat: 58.378254, lng: 26.714652};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDJMWyBmjbc_4VJUEUjeqYtQ5tnnwDs8Rk&callback=initMap">
+    </script>
+  </body>
+</html>
